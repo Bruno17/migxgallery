08ec04bebba404c0e468392f4826591af65cd3ce
Bruno Perner <b.perner@gmx.de>
2012-01-20 14:01:37 +0100

    initial commit
    Signed-off-by: Bruno Perner <b.perner@gmx.de>

---
+++ core/components/migxgallery/model/migxgallery/mysql/migxgallery.class.php
@@ -0,0 +1,3 @@
+<?php
+require_once (dirname(dirname(__FILE__)) . '/migxgallery.class.php');
+class migxGallery_mysql extends migxGallery {}
---
+++ core/components/migxgallery/model/migxgallery/mysql/migxgallery.map.inc.php
@@ -0,0 +1,166 @@
+<?php
+$xpdo_meta_map['migxGallery']= array (
+  'package' => 'migxgallery',
+  'table' => 'migx_gallery',
+  'fields' => 
+  array (
+    'title' => '',
+    'description' => NULL,
+    'resource_id' => 0,
+    'image' => '',
+    'published' => 0,
+    'createdby' => 0,
+    'createdon' => NULL,
+    'editedby' => 0,
+    'editedon' => NULL,
+    'deleted' => 0,
+    'deletedon' => NULL,
+    'deletedby' => 0,
+    'publishedon' => NULL,
+    'publishedby' => 0,
+  ),
+  'fieldMeta' => 
+  array (
+    'title' => 
+    array (
+      'dbtype' => 'varchar',
+      'precision' => '255',
+      'phptype' => 'string',
+      'null' => false,
+      'default' => '',
+      'index' => 'index',
+    ),
+    'description' => 
+    array (
+      'dbtype' => 'text',
+      'phptype' => 'string',
+      'index' => 'fulltext',
+    ),
+    'resource_id' => 
+    array (
+      'dbtype' => 'int',
+      'precision' => '11',
+      'phptype' => 'integer',
+      'null' => false,
+      'default' => 0,
+    ),
+    'image' => 
+    array (
+      'dbtype' => 'varchar',
+      'precision' => '255',
+      'phptype' => 'string',
+      'null' => false,
+      'default' => '',
+    ),
+    'published' => 
+    array (
+      'dbtype' => 'tinyint',
+      'precision' => '1',
+      'attributes' => 'unsigned',
+      'phptype' => 'integer',
+      'null' => false,
+      'default' => 0,
+    ),
+    'createdby' => 
+    array (
+      'dbtype' => 'int',
+      'precision' => '10',
+      'phptype' => 'integer',
+      'null' => false,
+      'default' => 0,
+    ),
+    'createdon' => 
+    array (
+      'dbtype' => 'datetime',
+      'phptype' => 'datetime',
+      'null' => false,
+    ),
+    'editedby' => 
+    array (
+      'dbtype' => 'int',
+      'precision' => '10',
+      'phptype' => 'integer',
+      'null' => false,
+      'default' => 0,
+    ),
+    'editedon' => 
+    array (
+      'dbtype' => 'datetime',
+      'phptype' => 'datetime',
+      'null' => false,
+    ),
+    'deleted' => 
+    array (
+      'dbtype' => 'tinyint',
+      'precision' => '1',
+      'attributes' => 'unsigned',
+      'phptype' => 'integer',
+      'null' => false,
+      'default' => 0,
+    ),
+    'deletedon' => 
+    array (
+      'dbtype' => 'datetime',
+      'phptype' => 'datetime',
+      'null' => false,
+    ),
+    'deletedby' => 
+    array (
+      'dbtype' => 'int',
+      'precision' => '10',
+      'phptype' => 'integer',
+      'null' => false,
+      'default' => 0,
+    ),
+    'publishedon' => 
+    array (
+      'dbtype' => 'datetime',
+      'phptype' => 'datetime',
+      'null' => false,
+    ),
+    'publishedby' => 
+    array (
+      'dbtype' => 'int',
+      'precision' => '10',
+      'phptype' => 'integer',
+      'null' => false,
+      'default' => 0,
+    ),
+  ),
+  'indexes' => 
+  array (
+    'PRIMARY' => 
+    array (
+      'alias' => 'PRIMARY',
+      'primary' => true,
+      'unique' => true,
+      'columns' => 
+      array (
+        'id' => 
+        array (
+          'collation' => 'A',
+          'null' => false,
+        ),
+      ),
+    ),
+  ),
+  'aggregates' => 
+  array (
+    'Resource' => 
+    array (
+      'class' => 'modResource',
+      'local' => 'resource_id',
+      'foreign' => 'id',
+      'cardinality' => 'one',
+      'owner' => 'foreign',
+    ),
+    'Creator' => 
+    array (
+      'class' => 'modUser',
+      'local' => 'createdby',
+      'foreign' => 'id',
+      'cardinality' => 'one',
+      'owner' => 'foreign',
+    ),
+  ),
+);
